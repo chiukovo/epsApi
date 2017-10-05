73c64bd86b3b22bdb59b40aa4820c07b69070752
chiukovo <q8156697@gmail.com>
2017-10-05 21:43:03 +0800

    fix bug

---
+++ app/Services/UserAuthServices.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services;
+
+class UserAuthServices
+{
+    /**
+     * do login
+     *
+     * @param array userPost
+     * @return array
+     */
+    public static function doLogin($userPost)
+    {
+        $yearSems = getYearSems();
+        $type = $userPost['type'];
+
+        if ( $type == 'student' ) {
+            $auth = studentAuth($userPost['username'], $userPost['password']);
+
+            if ($auth == 'Y') {
+                $student = getStudentData($userPost['username'], $yearSems['std_card_sems']);
+
+                if ( empty($student) ) {
+                    return [
+                        'status' => 'error',
+                        'msg' => '查無資料！請重新確認'
+                    ];
+                }
+
+                return [
+                    'status' => 'success',
+                    'msg' => '',
+                ];
+            }
+        } else {
+            //teacher wip
+            $teacher = getTeacherData($userPost['username'], $yearSems['std_card_sems']);
+
+            if ( empty($teacher) ) {
+                return [
+                    'status' => 'error',
+                    'msg' => '查無資料！請重新確認'
+                ];
+            }
+
+            if ($teacher['teacher_pwd'] == '0x' . md5($userPost['password'])) {
+                return [
+                    'status' => 'success',
+                    'msg' => '',
+                ];
+            } else {
+                return [
+                    'status' => 'error',
+                    'msg' => '密碼錯誤！請重新確認'
+                ];
+            }
+        }
+
+        return [
+            'status' => 'error',
+            'msg' => '帳號或密碼錯誤或身分錯誤！請重新確認'
+        ];
+    }
+}
