26ee012b85c4f2a229eb7ee1e22e456d7784fe7e
chiukovo <q8156697@gmail.com>
2017-10-11 13:21:29 +0800

    fix bug

--- app/Http/Controllers/Student/ApiController.php
+++ app/Http/Controllers/Student/ApiController.php
@@ -140,11 +140,11 @@ class ApiController extends Controller
             return [
                 'status' => 'success',
                 'data' => [
-                    'photo' => AlbumRepositories::getByFilters([
+                    AlbumRepositories::getByFilters([
                         'tbStu_Album.Folder_Name_Id' => $request['folderId'],
                         'tbStu_Album.Stu_Id' => $userId,
-                    ]),
-                ]
+                    ])
+                ],
             ];
         }
 
