d1eaac0c3f06ac6bab56c0afc7924965acf6fa06
chiukovo <q8156697@gmail.com>
2018-01-22 15:47:08 +0800

    change auth

--- app/Helpers/ApiAuthHelper.php
+++ app/Helpers/ApiAuthHelper.php
@@ -7,8 +7,8 @@ if (! function_exists('authApiField')) {
      */
     function authApiField($request)
     {
-        if ( isset($request['id']) && isset($request['token'])) {
-            $check = md5(env('APP_KEY') . $request['id']);
+        if ( isset($request['id']) && isset($request['type'] && isset($request['token'])) {
+            $check = md5(env('APP_KEY') . $request['id'] . $request['type'] . date('YmdH'));
 
             if ( $check == $request['token'] ) {
                 return true;
